ffa12bdf991adaf2d2bc4ac6ad163ffce01b9972
Sebastian Luczak <s.luczak@samsung.com>
2015-07-14 15:31:13 +0200

    streams example

--- Lesson1/Lesson1.cpp
+++ Lesson1/Lesson1.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
-#include <algorithm>
-#include "headerFile.h"
-#include "subfolder\otherHeaderFile.h"
 
 int main() {
-	std::cout << "Hello World" << std::endl;
-
-	int a = 2147483647; // assigned maximum value of int
-	std::cout << a << std::endl;
-	a = 2147483648; // results in rolling over to a = -2147483648;
-	std::cout << a << std::endl;
-
-	unsigned int b = 4294967295; // assigned maximum value of unsigned int
-	std::cout << b << std::endl;
-	b = -1; // results in rolling over to b = 4294967295;
-	std::cout << b << std::endl;
-
-
+	std::cout << "Hello, how old are you?" << std::endl;
+	int age;
+	std::cin >> age;
+	std::cout << "Your age is " << age << std::endl;
+	std::cout << "I " << "concatenate " << std::endl << "STREAMS" << "!" << std::endl;
 	return 0;
 }
