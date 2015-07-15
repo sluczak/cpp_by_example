695e9c324c6e07065004467e4cfbf12c74dd879c
Sebastian Luczak <s.luczak@samsung.com>
2015-07-15 12:48:36 +0200

    operators

--- Lesson1/Lesson1.cpp
+++ Lesson1/Lesson1.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-namespace differentNamespace {
-	int sth() {
-		cout << "some differentNamespace" << endl;
-		return 1;
-	}
-}
-
-namespace evenMoreDifferentNamespace {
-	int sth() {
-		cout << "some evenMoreDifferentNamespace" << endl;
-		return 1;
-	}
-}
-
-using evenMoreDifferentNamespace::sth;
-
 int main() {
-	differentNamespace::sth();
-	sth();
+	double a = 2;
+	int b = 5;
+	double result;
+
+	result = ((a + b) * (3 - 1)) / 5;
+
+	cout << "This is a result of double arithmetic operations " <<  result << endl;
+
+	int d = -b;
+
+	cout << "b was: " << b << " d is a negation of b: " << d << endl;
+
+	bool isFree = true;
+	bool isHungry = false;
+	bool isHappy = true;
+	if (isFree && !isHungry || isHappy) {
+		cout << "I am free, NOT hungry and happy OR unhappy!" << endl;
+	}
 	return 0;
 }
